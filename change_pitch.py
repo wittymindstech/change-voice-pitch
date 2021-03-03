15e854b6878e20e0121aa6c9ff0f43038cae336e
wiTTyMindsTech <63321726+wittymindstech@users.noreply.github.com>
2021-03-03 14:02:24 +0530

    Create change_pitch.py

---
+++ change_pitch.py
@@ -0,0 +1,24 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+sound = AudioSegment.from_file('in.wav', format="wav")
+
+# shift the pitch up by half an octave (speed will increase proportionally)
+octaves = 0.5
+
+new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+
+# keep the same samples but tell the computer they ought to be played at the 
+# new, higher sample rate. This file sounds like a chipmunk but has a weird sample rate.
+hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+
+# now we just convert it to a common sample rate (44.1k - standard audio CD) to 
+# make sure it works in regular audio players. Other than potentially losing audio quality (if
+# you set it too low - 44.1k is plenty) this should now noticeable change how the audio sounds.
+hipitch_sound = hipitch_sound.set_frame_rate(44100)
+
+#Play pitch changed sound
+play(hipitch_sound)
+
+#export / save pitch changed sound
+hipitch_sound.export("out.wav", format="wav")
